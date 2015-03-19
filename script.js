b7a74636423f86c0538a70a94d1550d45ffe4bc4
Shane Prendergast <contact@webknit.co.uk>
2015-03-19 17:18:26 +0000

    adding initial random shapes

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-/* - BASE HTML TEMPLATE
-------------------------------------------------- 
-	Description: JS Scripts
-	Author:Shane Prendergast
-	Author URL:http://www.webknit.co.uk
-	Template URL:http://base.webknit.co.uk/
-*/
+var canvas = canvas || {};
 
-// JS EXAMPLE
-
-var Base = Base || {};
-
-Base.functionName = function()
-{	
-	var self = $(this);
-	var variable = $('.var');
-
-	function init()
-	{
-		variable.click(functionOne);
-		functionTwo();
-	}
-
-	function functionOne()
-	{
-		// JS CODE
-	}
-
-	function functionTwo()
-	{
-		// JS CODE
-		window.hide();
-	}
-
-	init();
+canvas.element = document.getElementById('canvas');
+canvas.context = canvas.element.getContext('2d');
+canvas.width = canvas.element.getAttribute('width');
+canvas.height = canvas.element.getAttribute('height');
+canvas.values = {
+	fill: 'white',
+	stroke: 'black',
+	size: 10
 };
 
-// ON DOC READY
-$(function()
-{	
-	new Base.functionName();
-	
-});
+canvas.draw = function(x, y, fillColour, strokeColour) {
 
+	var x = x || 0;
+	var y = y || 0;
+	var fillColour = fillColour || 'black';
+	var strokeColour = strokeColour || 'white';
+
+	this.context.fillStyle = fillColour;
+	this.context.fillRect(x * canvas.values.size, y * canvas.values.size, this.width, this.height);
+
+	this.context.strokeStyle = strokeColour;
+	this.context.strokeRect(x * canvas.values.size, y * canvas.values.size, this.width, this.height);
+
+}
+
+// Draw the canvas
+canvas.draw(0, 0, 'white', 'black');
+
+// Define this in global scope
+var pixel;
+
+function drawPixel() {
+
+	pixel = {
+		x: Math.round(Math.random()*(canvas.width - canvas.values.size) / canvas.values.size),
+		y: Math.round(Math.random()*(canvas.height - canvas.values.size) / canvas.values.size)
+	};
+
+}
+
+drawPixel();
+canvas.draw(pixel.x, pixel.y, 'red', 'white');
