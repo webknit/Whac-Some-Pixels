3045b4f956cf8e5bea7c04bc21cd0d114f61b0be
Shane Prendergast <contact@webknit.co.uk>
2015-03-20 11:56:11 +0000

    working on click events on the canvas

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ var WhacSomePixels = (function () {
 		// Creates a random px
 		drawPixel();
 
-		// Draws the initial random pixel to the canvas
-		canvas.draw(pixel.x, pixel.y, 'red', 'white', canvas.values.pixelSize, canvas.values.pixelSize);
-
 	}
 
 	var canvas = canvas || {};
@@ -19,11 +16,13 @@ var WhacSomePixels = (function () {
 	canvas.context = canvas.element.getContext('2d');
 	canvas.width = canvas.element.getAttribute('width');
 	canvas.height = canvas.element.getAttribute('height');
+	canvas.elemLeft = canvas.element.offsetLeft,
+    canvas.elemTop = canvas.element.offsetTop,
 	canvas.values = {
 		fill: 'white',
 		stroke: 'black',
 		size: 1,
-		pixelSize: 10
+		pixelSize: 100
 	};
 
 	canvas.draw = function(x, y, fillColour, strokeColour, height, width) {
@@ -50,11 +49,31 @@ var WhacSomePixels = (function () {
 	function drawPixel() {
 
 		pixel = {
-			x: Math.round(Math.random()*(canvas.width - canvas.values.size) / canvas.values.size),
-			y: Math.round(Math.random()*(canvas.height - canvas.values.size) / canvas.values.size)
+			x: Math.round(Math.random() * (canvas.width - canvas.values.size) / canvas.values.size),
+			y: Math.round(Math.random() * (canvas.height - canvas.values.size) / canvas.values.size)
 		};
 
+		// Draws the initial random pixel to the canvas
+		canvas.draw(pixel.x, pixel.y, 'red', 'white', canvas.values.pixelSize, canvas.values.pixelSize);
+
 	}
+
+	canvas.element.addEventListener('click', function(event) {
+
+		console.log(pixel.x, pixel.y);
+
+		var x = event.pageX - canvas.elemLeft;
+        var y = event.pageY - canvas.elemTop;
+
+        if (x >+ pixel.x && x <= pixel.x + canvas.values.pixelSize || y >= pixel.y && y <= pixel.y + canvas.values.pixelSize) {
+
+        	drawPixel();
+
+        }
+
+        console.log(x, y);
+
+	}, false);
 
 	init();
 
